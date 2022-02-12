9f9f179be8856db654a4fc90cb99f6d491fe944c
njeri waweru <wawerunjeri714@gmail.com>
2022-02-12 23:46:32 +0300

    added footer component

--- quotemaker/src/app/app.module.ts
+++ quotemaker/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+;
+import { FooterComponent } from './footer/footer.component'import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CustomerUppercaseDirective } from './customer-uppercase.directives';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { formsComponent } from 
 
 @NgModule({
   declarations: [
     AppComponent,
     FormsComponent.
     QuoteDisplayComponent,
-    CustomerUppercaseDirective
+    CustomerUppercaseDirective,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
